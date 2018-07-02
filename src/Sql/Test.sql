5342e333f8b7b25e7d4906d73f39105716cc1e84
rhyous <rhyous@yahoo.com>
2018-07-02 14:57:07 -0600

    Added tests

--- src/Sql/Test.sql
+++ src/Sql/Test.sql
@@ -3,11 +3,17 @@ DECLARE @table NVARCHAR(100) = 'OrganizationFile'
 -- Test create dir
 EXEC CreateDirectory @table, 'A' 
 
+-- Test Directory Exists
+SELECT dbo.DirectoryExists(@table, 'A')
+
 -- Test create multiple dirs
 EXEC CreateDirectory @table, 'A\B\C' 
 
 -- Test create file
 EXEC CreateFile @table, 'A\B\C\hw.txt', 0x48656C6C6F2C20776F726C6421 
+
+-- Test FileExists
+SELECT dbo.FileExists(@table, 'A\B\C\hw.txt')
 
  -- Test Create dir and file
 EXEC CreateFile @table, 'A\B\C\D\hw.txt', 0x48656C6C6F2C20776F726C6421
@@ -17,11 +23,11 @@ EXEC CreateTextFile @table, 'A\B\C\D\hw2.txt', 'Hello, world!' -- Test Create di
 
 -- Test Create text file and delete it
 EXEC CreateTextFile @table, 'A\B\C\D\deleteByStreamId.txt', 'I must be deleted by stream_id.'
-DECLARE @stream_id UniqueIdentifier = (SELECT TOP 1 stream_Id from OrganizationFile where NAME = 'deleteByStreamId.txt')
+DECLARE @stream_id UniqueIdentifier = (SELECT TOP 1 stream_Id from @table where NAME = 'deleteByStreamId.txt')
 EXEC DeletefileByStreamId @table, @stream_id
 
 EXEC CreateTextFile @table, 'A\B\C\D\deleteByHierarchyId.txt', 'I must be deleted path_locator.'
-DECLARE @path_locator HierarchyId = (SELECT TOP 1 path_locator from OrganizationFile where NAME = 'deleteByHierarchyId.txt')
+DECLARE @path_locator HierarchyId = (SELECT TOP 1 path_locator from @table where NAME = 'deleteByHierarchyId.txt')
 EXEC DeletefileByPathLocator @table, @path_locator
 
 EXEC CreateTextFile @table, 'A\B\C\D\deleteByPath.txt', 'I must be deleted by full path.'
@@ -29,14 +35,12 @@ EXEC DeletefileByPath @table, 'A\B\C\D\deleteByPath.txt'
 
 -- Test Create text file and rename it
 EXEC CreateTextFile @table, 'A\B\C\D\RenameByStreamId.txt', 'I must be renamed by stream_id.'
-SET @stream_id = (SELECT TOP 1 stream_Id from OrganizationFile where NAME = 'RenameByStreamId.txt')
+SET @stream_id = (SELECT TOP 1 stream_Id from @table where NAME = 'RenameByStreamId.txt')
 EXEC RenamefileByStreamId @table, @stream_id, 'RenameByStreamId1.txt'
 
 EXEC CreateTextFile @table, 'A\B\C\D\RenameByHierarchyId.txt', 'I must be renamed path_locator.'
-SET @path_locator = (SELECT TOP 1 path_locator from OrganizationFile where NAME = 'RenameByHierarchyId.txt')
+SET @path_locator = (SELECT TOP 1 path_locator from @table where NAME = 'RenameByHierarchyId.txt')
 EXEC RenamefileByPathLocator @table, @path_locator, 'RenameByHierarchyId1.txt'
 
 EXEC CreateTextFile @table, 'A\B\C\D\RenameByPath.txt', 'I must be renamed by full path.'
 EXEC RenamefileByPath @table, 'A\B\C\D\RenameByPath.txt', 'RenameByPath1.txt'
-
-SELECT * FROM OrganizationFile
